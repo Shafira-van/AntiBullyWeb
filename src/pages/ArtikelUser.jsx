bf8b9e53f7799ecd7a7c579b42e8382092eed2a8
shafira <shafiravandeyli10@gmail.com>
2022-11-25 10:43:30 +0700

    update

--- src/pages/ArtikelUser.jsx
+++ src/pages/ArtikelUser.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import Artikel from '../components/Artikel'
 import DetailArtikel from '../components/DetailArtikel'
+import Navbar from '../components/Navbar'
 
 function ArtikelUser() {
   return (
     <div>
+      <Navbar/>
       <Artikel />
-      <DetailArtikel/>
+      <DetailArtikel />
+      <Footer/>
     </div>
   )
 }
