744d2ffe96dcdac22cb3c03a1df3ca1343a5dd0b
shafira <shafiravandeyli10@gmail.com>
2022-11-30 14:03:07 +0700

    update api

--- src/pages/ArtikelPsikolog.jsx
+++ src/pages/ArtikelPsikolog.jsx
@@ -2,14 +2,17 @@ import Artikel from '../components/Artikel'
 import React, { useState } from "react";
 
 import AddArtikel from '../components/AddArtikel';
+import { Link } from 'react-router-dom';
 
 
 function ArtikelPsikolog() {
   
 
   return (
-    <div>
-      <AddArtikel/>
+    <div className="kontenKomponen">
+      <Link to={`addartikel`}>
+        <button className="btnAddArtikel">Add Artikel+</button>
+      </Link>
       <Artikel />
     </div>
   );
